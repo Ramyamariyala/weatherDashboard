e15a0b0b72ddec34219494bb12776eca680d24e8
Ramya.Mariyala <ramya.mariyala@gmail.com>
2021-01-30 23:03:56 -0500

    cannot finish

---
+++ Script.js
@@ -0,0 +1,16 @@
+var searchBtn = $(".searchBtn");
+var searchInput = $("searchInput");
+
+
+var cityNameEl = $(".cityName");
+var currrentDateEl = $(".currentDate");
+var weatherIconEl = $(".weatherIcon");
+var serchHistoryEl = $(".historyItems");
+
+var tempEl = $(".temp");
+var humidityEl = $(".humidity");
+var windSpeedEl = $(".windSpeed");
+var uvIndexEl = $(".uvIndex");
+var cardRow = $(".card-row");
+
+searchBtn.on("click",function(e))
